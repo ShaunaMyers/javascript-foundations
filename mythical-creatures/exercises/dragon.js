d51340926bf3b0487d01d0e90412f8587e950fb9
Shauna Myers <shaunamyers26@gmail.com>
2021-03-30 23:46:50 -0400

    Update and complete dragon.js exercises

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+  constructor(dragonName, dragonJockey) {
+    this.name = dragonName;
+    this.rider = dragonJockey;
+    this.hungry = true;
+    this.meals = 0;
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    this.meals++;
+    if (this.meals >= 3) {
+      this.hungry = false;
+    } else {
+      this.hungry = true;
+    }
+  }
+};
 
+module.exports = Dragon;
